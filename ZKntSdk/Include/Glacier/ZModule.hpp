8be468a1a8b16f119258d9a2f80737d97450d251
Pavle <pavle_nis@yahoo.com>
2026-06-10 19:49:48 +0200

    Add IsEngineInitialized method to ZGameSceneflowModule

--- ZKntSdk/Include/Glacier/ZModule.hpp
+++ ZKntSdk/Include/Glacier/ZModule.hpp
@@ -9,6 +9,15 @@ class ZSimpleModuleBase : public IModule {};
 
 class ZGameSceneflowModule : public ZSimpleModuleBase {
   public:
+    [[nodiscard]]
+    bool IsEngineInitialized() const {
+        if (!m_pEntitySceneContext) {
+            return false;
+        }
+
+        return m_pEntitySceneContext->m_SceneResource.size() != 0;
+    }
+
     PAD(0x58);
     ZEntitySceneContext* m_pEntitySceneContext; // 0x60
 };
